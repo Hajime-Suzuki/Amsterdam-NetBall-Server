49658b5d94bc422fe61b8c04005ff3cd388ad7c0
Jackson Moore <jacksonmoore@tuta.io>
2018-08-27 13:34:59 +0200

    minimum Member controller

---
+++ src/controllers/MemberController.ts
@@ -0,0 +1,34 @@
+import { JsonController } from 'routing-controllers'
+import Member from './entity';
+// , Post, Param, Get, Body, Authorized 
+
+@JsonController()
+export default class MemberController {
+
+  @Post('/signup')
+  async signup(
+    @Body() data: Member
+  ) {
+    const {password, ...rest} = data
+    const entity = Member.create(rest)
+    await entity.setPassword(password)
+
+    const user = await entity.save()
+
+    return user
+  }
+
+  @Authorized()
+  @Get('/members/:id([0-9]+)')
+  getUser(
+    @Param('id') id: number
+  ) {
+    return Member.findOne(id)
+  }
+
+  @Authorized()
+  @Get('/members')
+  allUsers() {
+    return Member.find()
+  }
+}
