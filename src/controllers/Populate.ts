d12143b439eafc28f8146db8aeaf5b40196434cf
Hajime Suzuki <hajime.doublebass@gmail.com>
2018-08-27 15:37:41 +0200

    Checking: Add member

--- src/controllers/Populate.ts
+++ src/controllers/Populate.ts
@@ -12,10 +12,25 @@ import * as faker from 'faker'
 @JsonController('/populate')
 export default class PopulateController {
   @Get('/')
-  populate() {
-    // const member = Member.create({
-    //   name: faker.name.
-    // })
-    return 'ashiotenaisehotn'
+  async populate() {
+    const startDate = faker.date.between('2017-01-01', new Date())
+
+    const endDate = faker.date.between(startDate, '2019-01-01')
+
+    const member = await Member.create({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      streetAddress: faker.address.streetAddress(),
+      postalCode: faker.address.zipCode('######'),
+      city: faker.address.city(),
+      dateOfBirth: faker.date.between('1980-01-01', '2000-01-01'),
+      isCurrentMember: false,
+      email: faker.internet.email(),
+      password: 'aaaaaa',
+      startDate,
+      endDate
+    }).save()
+
+    return member
   }
 }
