4e637b23003d7da6dc7fb9a6d88fe61f42497008
timvandervoorn <tvanderv@gmail.com>
2018-08-29 10:21:59 +0200

    Members search bar

--- src/controllers/MemberController.ts
+++ src/controllers/MemberController.ts
@@ -8,77 +8,92 @@ import {
   CurrentUser,
   QueryParam,
   BadRequestError
-} from 'routing-controllers'
-import { getRepository } from 'typeorm'
-import { Member } from '../entities/Member'
-import { Position } from '../entities/Position'
+} from "routing-controllers"
+import { getRepository } from "typeorm"
+import { Member } from "../entities/Member"
+import { Position } from "../entities/Position"
 
 @JsonController()
 export default class MemberController {
-  @Post('/signup')
+  @Post("/signup")
   signup(@Body() data: Member) {
     console.log(data)
     return Member.create(data).save()
   }
 
   @Authorized()
-  @Get('/members/:id([0-9]+)')
-  getUser(
-    @Param('id') id: number,
-    @CurrentUser() user: Member
-  ) {
+  @Get("/members/:id([0-9]+)")
+  getUser(@Param("id") id: number, @CurrentUser() user: Member) {
     const member = Member.findOne(id)
     return member
   }
 
-
   // @Authorized()
-  @Get('/members')
+  @Get("/members")
   async allUsers(
-    @QueryParam('id') id: string,
-    @QueryParam('name') name: string,
-    @QueryParam('email') email: string,
-    @QueryParam('city') city: string,
-    @QueryParam('isCurrentMember') isCurrentMember: string,
-    @QueryParam('dateOfBirth') dateOfBirth: string,
-    @QueryParam('startDate') startDate: string,
-    @QueryParam('endDate') endDate: string,
-    @QueryParam('positions') positions: string,
-    @QueryParam('team') team: string,
-    @QueryParam('role') role: string,
+    @QueryParam("id") id: string,
+    @QueryParam("name") name: string,
+    @QueryParam("email") email: string,
+    @QueryParam("city") city: string,
+    @QueryParam("isCurrentMember") isCurrentMember: string,
+    @QueryParam("dateOfBirth") dateOfBirth: string,
+    @QueryParam("startDate") startDate: string,
+    @QueryParam("endDate") endDate: string,
+    @QueryParam("positions") positions: string,
+    @QueryParam("team") team: string,
+    @QueryParam("role") role: string,
     @CurrentUser() user: Member | null
   ) {
- 
-    let query = Member.createQueryBuilder("member").leftJoinAndSelect("member.positions", "positions")
+    let query = Member.createQueryBuilder("member").leftJoinAndSelect(
+      "member.positions",
+      "positions"
+    )
 
     if (name) {
-      query = query.where("member.firstName LIKE :name OR member.lastName LIKE :name", { name: `%${name}%` })
+      query = query.where(
+        "member.firstName LIKE :name OR member.lastName LIKE :name",
+        { name: `%${name}%` }
+      )
     }
     if (email) {
-      query = query.andWhere("member.email LIKE :email", { email: `%${email}%` })
+      query = query.andWhere("member.email LIKE :email", {
+        email: `%${email}%`
+      })
     }
     if (city) {
       query = query.andWhere("member.city LIKE :city", { city: `%${city}%` })
     }
     if (isCurrentMember) {
-      query = query.andWhere("member.isCurrentMember = :isCurrentMember", { isCurrentMember })
+      query = query.andWhere("member.isCurrentMember = :isCurrentMember", {
+        isCurrentMember
+      })
     }
     if (dateOfBirth) {
       const timestamp = new Date(dateOfBirth)
-      query = query.andWhere("member.dateOfBirth = :dateOfBirth", { dateOfBirth: timestamp })
+      query = query.andWhere("member.dateOfBirth = :dateOfBirth", {
+        dateOfBirth: timestamp
+      })
     }
     if (startDate) {
       const timestamp = new Date(startDate)
-      query = query.andWhere("member.startDate = :startDate", { startDate: timestamp })
+      query = query.andWhere("member.startDate = :startDate", {
+        startDate: timestamp
+      })
     }
     if (endDate) {
       const timestamp = new Date(endDate)
-      query = query.andWhere("member.endDate = :endDate", { endDate: timestamp })
+      query = query.andWhere("member.endDate = :endDate", {
+        endDate: timestamp
+      })
     }
+
+    console.log("MEMBERS CONTROLLER RUNS BEFORE SPLIT ERROR!")
     if (positions) {
-      const allPositions = positions.split(',')
-      console.log('positions', allPositions)
-      query = query.andWhere("positions.id = :position", { position: allPositions[0] })
+      const allPositions = positions.split(",")
+      console.log("positions", allPositions)
+      query = query.andWhere("positions.id = :position", {
+        position: allPositions[0]
+      })
     }
     if (team) {
       query = query.where("member.team.id = :team", { team: team })
@@ -88,5 +103,4 @@ export default class MemberController {
     }
     return query.getMany()
   }
-
 }
