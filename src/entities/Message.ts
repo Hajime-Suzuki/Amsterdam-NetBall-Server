ddab7548ed71d87485cb4918807d143ef864bfcc
Jackson Moore <jacksonmoore@tuta.io>
2018-08-30 11:23:36 +0200

    committee / message endpoints

---
+++ src/entities/Message.ts
@@ -0,0 +1,21 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne
+} from 'typeorm'
+import { Committee } from './Committee'
+
+@Entity()
+export class Message extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column('text')
+  body: string
+
+  @ManyToOne(() => Committee, committee => committee.messages)
+  committee: Committee
+
+}
